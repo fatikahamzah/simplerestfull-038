cb51a0c49f14eff6c8259f9d1cc6e109fe10423e
fatikahamzah <fatikahmzptr@gmail.com>
2022-10-18 19:24:44 +0700

    First Commit

---
+++ Documents/NetBeansProjects/Perpustakaan/src/test/java/perpustakaan/Perpustakaan/PerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package perpustakaan.Perpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
