5c32c9b8df6162f86a3a5a6502d3ed261bb95a8a
fatikahamzah <fatikahmzptr@gmail.com>
2022-10-19 13:41:34 +0700

    First Commit

--- Documents/NetBeansProjects/Perpustakaan/src/main/java/PWS/Perpustakaan/PerpustakaanApplication.java
+++ Documents/NetBeansProjects/Perpustakaan/src/main/java/PWS/Perpustakaan/PerpustakaanApplication.java
@@ -1,4 +1,4 @@
-package perpustakaan.Perpustakaan;
+package PWS.Perpustakaan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Documents/NetBeansProjects/Perpustakaan/src/test/java/PWS/Perpustakaan/PerpustakaanApplicationTests.java
+++ Documents/NetBeansProjects/Perpustakaan/src/test/java/PWS/Perpustakaan/PerpustakaanApplicationTests.java
@@ -1,4 +1,4 @@
-package perpustakaan.Perpustakaan;
+package PWS.Perpustakaan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
