46ab1502e8f06920417d31982727d4a91903a457
fatikahamzah <fatikahmzptr@gmail.com>
2022-10-31 19:49:29 +0700

    initial commit

---
+++ Documents/NetBeansProjects/InitializrSpringbootProject/src/test/java/pws/data/fatika/FatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.data.fatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
