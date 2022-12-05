0e43188b07e098057052924334eb4bb7be9da071
fatikahamzah <fatikahmzptr@gmail.com>
2022-12-05 17:37:35 +0700

    controller and html

---
+++ Documents/NetBeansProjects/Pws_B/src/main/java/ws/b/project04/ProductExceptionController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.b.project04;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+/**
+ *
+ * @author asus
+ */
+
+@ControllerAdvice
+public class ProductExceptionController {
+   @ExceptionHandler(value = ProductNotFoundException.class)
+   public ResponseEntity<Object> exception(ProductNotFoundException exception) {
+      return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+   }
+}
